f23038f1c2e14ccfd5af81ab439b15dab647eaef
407 <407@academicos.cesde.edu.co>
2022-09-27 17:36:00 -0500

    se crea app de React

---
+++ src/Componente/Andrea.js
@@ -0,0 +1,14 @@
+export function Andrea(){
+
+    let nombre="Andrea"
+    //interpoolar=controlar la GUI desde JS
+
+    return(
+        <>
+            <h1>HOLA REACT SOY {nombre} </h1>
+            <hr/>       
+            
+        </>
+        
+    )
+}
---
+++ src/Sinopsis/Sinopsis.js
@@ -0,0 +1,11 @@
+export function Sinopsis(){
+    return(
+        <>
+            <p>
+            Los humanos se encuentran en conflicto con los nativos del clan Omaticaya debido a que están asentados alrededor de un gigantesco árbol, conocido por ellos como Árbol Madre, que cubre una inmensa veta de un mineral muy cotizado: el unobtanium. La existencia de dicho mineral ha llevado a una empresa privada a crear un proyecto de explotación de recursos minerales, dirigido por Parker Selfridge en lo civil y por el coronel Miles Quaritch en lo militar.
+            </p>
+        </>
+    )
+
+        
+}
